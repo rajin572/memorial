6d3232b68a070dde99b91f588f08febc788e19ff
Md. Sohag Hossain <sohaghossain7th@gmail.com>
2025-01-18 20:16:08 +0600

    update url

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,23 @@
-import createNextIntlPlugin from 'next-intl/plugin';  
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "192.168.10.205", "https://memorial.resid-plus.com/api/v1", "memorial.resid-plus.com"],
-   
+    domains: [
+      "localhost",
+      "192.168.10.205",
+      "167.172.237.118",
+      "https://api.memorialmoments.org/api/v1",
+      "api.memorialmoments.org",
+    ],
   },
   server: {
-    host: '0.0.0.0',  // This allows access from the local network
-    port: 3000,       // Optional: You can specify the port if needed
+    host: "167.172.237.118", // This allows access from the local network
+    port: 3000, // Optional: You can specify the port if needed
   },
-  
 };
-
-
 
 // export default nextConfig;
 export default withNextIntl(nextConfig);
